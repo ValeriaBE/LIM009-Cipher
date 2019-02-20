c05d41b7001e6505f8acc0a56185dfc57ea67412
Valeria Berrocal <valeriaberrocal@MacBook-Pro-de-Valeria-2.local>
2019-02-20 11:53:18 -0500

    n

--- src/index.js
+++ src/index.js
@@ -42,7 +42,24 @@ cifer.addEventListener("click", () =>{
     results.style.display="block";
     resultsp.style.display ="block";
     document.getElementById("resultsp").innerHTML;
-    encode.call();
+    /* toma el valor del input y el offset*/
+    const message = document.getElementById("message").value;
+    const offset = document.getElementById("offset").value;
+
+    /*convertir el offset en un integer*/
+    let offnum = parseInt(offset, 10);
+
+    /*definir el alphabeto*/
+    const alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    /*implementar un for para que recorre todo el texto*/
+    for(let i=0; i< message.length; i++){
+      let c = message.charCodeAt(i);
+      if(c >= 65 && c <=  90) {
+        result += String.fromCharCode((c - 65 + shift) % 26 + 65);
+      }
+  }
+  console.log(result);
 })
 
 /*función al botón paciente*/
