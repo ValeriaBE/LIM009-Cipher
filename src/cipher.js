8aab647fb01eecb3ee62479cd0d4ecb2e447f7c2
Valeria Berrocal <valeriaberrocal@MacBook-Pro-de-Valeria-2.local>
2019-02-25 12:28:45 -0500

    jbj,b

--- src/cipher.js
+++ src/cipher.js
@@ -13,10 +13,10 @@ window.cipher = {
     /*implementar un for para que recorre todo el texto*/
     for(let i=0; i< message.length; i++){
     let valorAsccideLetra = message.charCodeAt(i);
-    /*if(valorAsccideLetra>=65 && valorAsccideLetra<=90){*/
+    if(valorAsccideLetra>=65 && valorAsccideLetra<=90){
     resultado += String.fromCharCode((valorAsccideLetra - 65 + offsetn) % 26 + 65);
-   /* }
-    if(valorAsccideLetra>=97 && valorAsccideLetra<=122){
+   }
+    /*if(valorAsccideLetra>=97 && valorAsccideLetra<=122){
       resultado += String.fromCharCode((valorAsccideLetra - 97 + offsetn) % 26 + 97);
     }*/
     } 
@@ -33,10 +33,10 @@ window.cipher = {
     /*implementar un for y definir la formula*/
     for(let j=0; j<mCiphered.length; j++){
       let valor = mCiphered.charCodeAt(j);
-      /*if(valor>=65 && valor<=90){*/
+      if(valor>=65 && valor<=90){
       resultadoDecode += String.fromCharCode((valor - offset2n + 65) % 26 + 65);
-      /*}
-      if(valor>=97 && valor<=122){
+      }
+      /*if(valor>=97 && valor<=122){
         resultadoDecode += String.fromCharCode((valor-offset2n + 33) % 26 +97);
       }*/
     }
