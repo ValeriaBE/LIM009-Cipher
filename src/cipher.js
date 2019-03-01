af57705bfb4e98487e676d884ca26e158fc4bde1
Valeria Berrocal <valeriaberrocal@MacBook-Pro-de-Valeria-2.local>
2019-03-01 10:13:54 -0500

    añadiendotodos los tests y hacker edition

--- src/cipher.js
+++ src/cipher.js
@@ -6,21 +6,50 @@ window.cipher = {
 
     /*implementar un for para que recorre todo el texto*/
     for(let i=0; i< message.length; i++){
-    let valorAsccideLetra = message.charCodeAt(i);
+      let valorAsccideLetra = message.charCodeAt(i);
+    if(offsetn>=0){
     if(valorAsccideLetra>=65 && valorAsccideLetra<=90){
     resultado += String.fromCharCode((valorAsccideLetra - 65 + offsetn) % 26 + 65);
     }
      if(valorAsccideLetra>=97 && valorAsccideLetra<=122){
       resultado += String.fromCharCode((valorAsccideLetra - 97 + offsetn) % 26 + 97);
     }
-    if(valorAsccideLetra==32){
+    if(valorAsccideLetra>=32 && valorAsccideLetra<65){
       resultado+= String.fromCharCode(valorAsccideLetra);
     }
-    if(valorAsccideLetra==44){
+    if(valorAsccideLetra>=191&& valorAsccideLetra<=252){
       resultado+= String.fromCharCode(valorAsccideLetra);
     }
+    }else{
 
+    if(valorAsccideLetra>=65 && valorAsccideLetra<=90 && offsetn<0){
+      if((valorAsccideLetra - 65 +offsetn) % 26>0){
+      resultado += String.fromCharCode(((valorAsccideLetra + 65 +offsetn) % 26)+ 65);
+      }
+      if((valorAsccideLetra - 65 +offsetn) % 26<0){
+        resultado += String.fromCharCode((((valorAsccideLetra - 65 +offsetn) % 26)+26) + 65);
+        }
+       if((valorAsccideLetra + 65 +offsetn) % 26==0){
+          resultado += String.fromCharCode(((valorAsccideLetra + 65 +offsetn) % 26) + 65);
+          }
+    } 
+    if(valorAsccideLetra>=97 && valorAsccideLetra<=122 && offsetn<0){
+      if((valorAsccideLetra - 97 +offsetn) % 26>0){
+      resultado += String.fromCharCode(((valorAsccideLetra - 97 +offsetn) % 26)+ 97);
+      }
+      if((valorAsccideLetra - 97 +offsetn) % 26<0){
+        resultado += String.fromCharCode((((valorAsccideLetra - 97 +offsetn) % 26)+26)+97);
+        }
+       if((valorAsccideLetra - 97 +offsetn) % 26==0){
+          resultado += String.fromCharCode(((valorAsccideLetra -97 +offsetn) % 26) + 97);
+          }
+    } 
+    
+    if(valorAsccideLetra>=191 && valorAsccideLetra<=252 && offsetn<0){
+      resultado+=String.fromCharCode(valorAsccideLetra);
+    }
   }
+}
     return (resultado);
   },
   decode: (mCiphered, offset2n) => {
@@ -31,22 +60,45 @@ window.cipher = {
     /*implementar un for y definir la formula*/
     for(let j=0; j<mCiphered.length; j++){
       let valor = mCiphered.charCodeAt(j);
+    if(offset2n>=0){
       if(valor>=65 && valor<=90){
       resultadoDecode += String.fromCharCode((valor - offset2n + 65) % 26 + 65);
       }
       if(valor>=97 && valor<=122){
         resultadoDecode += String.fromCharCode((valor-offset2n + 33) % 26 +97);
       }
-      if(valor==32){
+      if(valor>=32 &&  valor<65){
         resultadoDecode+= String.fromCharCode(valor);
       }
-      if(valor==44){
+      if(valor>=191 && valor<=252){
         resultadoDecode+= String.fromCharCode(valor);
       }
-    
-    }
-  
-    return (resultadoDecode);
+    }else{
 
+        if(valor>=65 && valor<=90 && offset2n<0){
+          if((valor - offset2n+65) % 26>0){
+          resultadoDecode += String.fromCharCode(((valor - offset2n +65) % 26)+ 65);
+          } 
+        if((valor - offset2n+65) % 26==0){
+          resultadoDecode += String.fromCharCode(((valor -offset2n +65) % 26) + 65);
+          } 
+        }
+        if(valor>=97 && valor<=122 && offset2n<0){
+          if((valor - offset2n +33) % 26>0){
+          resultadoDecode += String.fromCharCode(((valor - offset2n +33) % 26)+ 97);
+          }
+           if((valor - offset2n+33) % 26==0){
+              resultadoDecode += String.fromCharCode(((valor -offset2n+33) % 26) + 97);
+              }
+        } 
+        
+        if(valor>=191 && valor<=252 && offset2n<0){
+          resultadoDecode+=String.fromCharCode(valor);
+        }
+    
+      }
+
+    }
+    return (resultadoDecode);
   }
 };
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -18,14 +18,29 @@ describe('cipher', () => {
       assert.equal(cipher.encode("abcdefghijklmnopqrstuvwxyz", 33), "hijklmnopqrstuvwxyzabcdefg");
     });
 
-    it('debería retornar " " para " " con offset 33', ()=>{
-      assert.equal(cipher.encode(" ", 33), " ");
-    });
-
-    it('debería retornar "," para "," con offset 33', ()=>{
-      assert.equal(cipher.encode(",", 33), ",");
+    it('debería retornar " !#$%&()*+,-./0123456789:;<=>?@" para " !#$%&()*+,-./0123456789:;<=>?@" con offset 33', ()=>{
+      assert.equal(cipher.encode(" !#$%&()*+,-./0123456789:;<=>?@", 33), " !#$%&()*+,-./0123456789:;<=>?@");
     });
  
+    it('debería retornar "¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûü" para "¿¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûü" con offset 33', () =>{
+      assert.equal(cipher.encode("¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûü",33),"¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûü");
+    });
+    it('debería retornar "¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûü" para "¿¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûü" con offset -33', () =>{
+      assert.equal(cipher.encode("¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûü",-33),"¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûü");
+    });
+
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset -33', ()=>{
+      assert.equal(cipher.encode('HIJKLMNOPQRSTUVWXYZABCDEFG', -33), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    });
+    it('debería retornar "GNKZ" para "HOLA" con offset -1', ()=>{
+      assert.equal(cipher.encode('HOLA', -1), 'GNKZ');
+    });
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offset -33', ()=>{
+      assert.equal(cipher.encode('hijklmnopqrstuvwxyzabcdefg', -33), 'abcdefghijklmnopqrstuvwxyz');
+    });
+    it('debería retornar "gnkz" para "hola" con offset -1', ()=>{
+      assert.equal(cipher.encode('hola', -1), 'gnkz');
+    });
   });
 
   describe('cipher.decode', () => {
@@ -42,14 +57,21 @@ describe('cipher', () => {
       assert.equal(cipher.decode("hijklmnopqrstuvwxyzabcdefg", 33),"abcdefghijklmnopqrstuvwxyz" );
     });
 
-    it('debería retornar " " para " " con offset 33', ()=>{
-      assert.equal(cipher.decode(" ", 33), " ");
+    it('debería retornar " !#$%&()*+,-./0123456789:;<=>?@" para " !#$%&()*+,-./0123456789:;<=>?@" con offset 33', ()=>{
+      assert.equal(cipher.decode(" !#$%&()*+,-./0123456789:;<=>?@", 33), " !#$%&()*+,-./0123456789:;<=>?@");
     });
-
-    it('debería retornar "," para "," con offset 33', ()=>{
-      assert.equal(cipher.decode(",", 33), ",");
+    it('debería retornar "¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûü" para "¿¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûü" con offset 33', () =>{
+      assert.equal(cipher.decode("¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûü",33),"¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûü");
     });
-
+    it('debería retornar "¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûü" para "¿¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûü" con offset -33', () =>{
+      assert.equal(cipher.decode("¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûü",-33),"¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûü");
+    });
+    it('debería retornar "OPQRSTUVWXYZABCDEFGHIJKLMN" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset -33', ()=>{
+      assert.equal(cipher.decode('HIJKLMNOPQRSTUVWXYZABCDEFG', -33), 'OPQRSTUVWXYZABCDEFGHIJKLMN');
+    });
+    it('debería retornar "opqrstuvwxyzabcdefghijklmn" para "hijklmnopqrstuvwxyzabcdefg" con offset -33', ()=>{
+      assert.equal(cipher.decode('hijklmnopqrstuvwxyzabcdefg', -33), 'opqrstuvwxyzabcdefghijklmn');
+    });
   });
 
 });
